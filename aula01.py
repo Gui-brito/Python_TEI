583631c7b9f15865810b5509e149a8a9b1cfee70
Guilherme Moisés de Oliveira Brito <guilherme.brito@midassolutions.com.br>
2021-08-18 00:21:17 -0300

    Exercicios da aula 01 - Parcial

---
+++ aula01.py
@@ -0,0 +1,45 @@
+import os
+os.system('cls')
+"""
+Exercicio 1 - Crie uma aplicação que receba o valor da base e da altura 
+de um triângulo retângulo e apresente na tela sua área
+
+"""
+altura = float(input("Qual é a altura do triangulo, em centimetros? "))
+base = float(input("E qual é a base, também em centimetros? "))
+
+area = (altura*base)/2
+
+print("Fiz as contas aqui e a area do seu triangulo é %.2f\n\n" %area)
+input("Pressione qualquer tecla para o próximo exercicio...")
+
+os.system('cls')
+"""
+Exercicio 2 - Crie um programa para calcular e exibir na tela o peso ideal. IMC = (peso / (altura^2)
+
+"""
+altura_imc = float(input("Qual é a sua altura? em metros"))
+peso_imc = float(input("Qual é o seu peso? Em kg "))
+
+aux = altura_imc*altura_imc
+print(aux)
+imc = (peso_imc/aux)
+
+print("Fiz as contas aqui e o seu IMC é %.2f\n\n" %imc)
+if(imc < 18.5):
+    print("Você está abaixo do peso")
+elif(imc < 25):
+    print("Você está no peso ideal")
+elif(imc < 30):
+    print("Você está levemente acima do peso")
+elif(imc < 35):
+    print("Obesidade I")
+elif(imc < 40):
+    print("Obesidade II (Severa)")
+else:
+    print("Obesidade III (Mórbida)")
+
+
+input("Pressione qualquer tecla para o próximo exercicio...")
+
+os.system('cls')
--- teste.py
+++ teste.py
@@ -1,13 +1,23 @@
-msg = "Olá mundo!"
+import os
+os.system('cls')
+
+s = "aeiou"
+print(s[-2])
+print(s[0]*20)
+print("a" in s)
+print("x" not in s)
+
+
+msg = 2**3
 print(msg)
 
-nome = input("Entre com o nome: ")
+nome = input("entre com o nome: ")
 print(type(nome))
 print(nome)
 nome = 99
 print(type(nome))
 print(nome)
 
-a = True
-b = False
+a = true
+b = false
 print(a or b)
